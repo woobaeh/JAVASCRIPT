6a342a43e29448cd885a335014e85be4d56de9db
woobaeh <qozkth@gmail.com>
2022-03-26 00:26:02 +0900

    update

---
+++ 5.boolean.js
@@ -0,0 +1,18 @@
+let isFree = true;
+let isActivated = false;
+let isEnrolled = true;
+console.log(isEnrolled);
+console.clear();
+// Falshy 거짓이 값
+console.log(!!0);
+console.log(!!-0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!NaN);
+// Truthy 참인 값
+console.log(!!1);
+console.log(!!-1);
+console.log(!!"text");
+console.log(!!{});
+console.log(!!Infinity);
