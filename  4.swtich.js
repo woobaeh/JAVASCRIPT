1d1a485b3a698f29aff8ca1e9bb93d4467204f6a
woobaeh <qozkth@gmail.com>
2022-04-04 09:50:40 +0900

    Create  4.swtich.js

---
+++  4.swtich.js
@@ -0,0 +1,59 @@
+// 조건문 Conditinal Statement
+// switch
+// if else if else if else
+// 정해진 범위안의 값에 대해 특정한 일을 해야 하는 경우
+let day = 7; // 0:월요일 1:화요일...6:일요일
+
+let dayName;
+
+if (day === 0) {
+  dayName = '월요일';
+} else if (day === 1) {
+  dayName = '화요일';
+}
+
+switch (day) {
+  case 0:
+    dayName = '월요일';
+    break;
+  case 1:
+    dayName = '화요일';
+    break;
+
+  case 2:
+    dayName = '수요일';
+    break;
+
+  case 3:
+    dayName = '목요일';
+    break;
+
+  case 4:
+    dayName = '금요일';
+    break;
+
+  case 5:
+    dayName = '토요일';
+    break;
+
+  case 6:
+    dayName = '일요일';
+    break;
+  default:
+    console.log("can't find the day");
+}
+
+console.log(dayName);
+
+let condition = 'bad'; // okay, good -> 좋음! , bad -> 나쁨!
+let text;
+switch (condition) {
+  case 'okay':
+  case 'good':
+    text = '좋음!';
+    break;
+  case 'bad':
+    text = '나쁨!';
+    break;
+}
+console.log(text);
