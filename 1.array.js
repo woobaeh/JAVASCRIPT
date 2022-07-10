7a3ab1487f1b3ba7b56578d921d4e1cfa5d3e9d5
woobaeh <qozkth@gmail.com>
2022-07-10 22:07:01 +0900

    ways of create array

--- 1.array.js
+++ 1.array.js
@@ -1,12 +1,30 @@
 // 배열 생성 방법
-let array = new Array(3);
-console.log(array);
+// let array = new Array(3);
+// console.log(array);
 
-array = new Array(1, 2, 3);
-console.log(array);
+// array = new Array(1, 2, 3); // class 생성자
+// console.log(array);
 
-array = Array.of(1, 2, 3, 4)
-console.log(array);
+// array = Array.of(1, 2, 3, 4) // static 함수
+// console.log(array);
 
-const anotherArray = [1, 2, 3, 4]
+const anotherArray = [1, 2, 3, 4] // array literal
 console.log(anotherArray);
+
+array = Array.from(anotherArray) // static 함수
+console.log(array);
+
+
+// 일반적으로 배열은 동일한 메모리 크기를 가지며, 연속적으로 이어져 있어야 함
+// 하지만 자바스크립트에서의 배열은 연속적으로 이어져 있지 않고
+// 오브젝트와 유사함!
+// 자바스크립트의 배열은 일반적인 배열의 동작을 흉내낸 특수한 객체다!
+// 이걸 보완하기 위해서 타입이 정해져 있는 타입 배열이 있음 (Typed Collections)
+
+array = Array.from({
+  0: '안',
+  1: '녕',
+  length: 4,
+});
+
+console.log(array);
