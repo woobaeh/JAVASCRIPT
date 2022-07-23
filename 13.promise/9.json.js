e78133f9861e9a75f2324c5c796670c44c241ecd
woobaeh <qozkth@gmail.com>
2022-07-23 21:22:39 +0900

    review fetch

--- 13.promise/9.json.js
+++ 13.promise/9.json.js
@@ -11,8 +11,11 @@ const woo = {
   },
 };
 
+// 직렬화 Serializing: 객체를 문자열로 변환
 const json = JSON.stringify(woo);
 console.log(json);
 console.log(woo);
+
+// 역직렬화 Deserializing: 문자열 데이터를 자바스크립트 객체로 변환
 const parsed = JSON.parse(json);
 console.log(parsed);
