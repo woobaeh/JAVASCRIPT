8f76e995a504eb1e018092675df8478ccfc88cdd
woobaeh <qozkth@gmail.com>
2022-07-26 16:52:31 +0900

    prototype seal, preventExtensions

--- 2.freeze.js
+++ 2.freeze.js
@@ -11,5 +11,29 @@ delete dog.name;
 console.log(dog);
 woo.name = '혀누';
 console.log(dog);
+console.clear();
 
-// 밀봉! Object.seal
+// 밀봉! Object.seal : 값의 수정 ✅, 추가 ❌, 삭제 ❌, 속성 재정의 ❌
+const cat = { ...dog };
+// Object.assign(cat, dog);
+Object.seal(cat);
+console.log(cat);
+cat.name = '냐옹';
+console.log(cat);
+delete cat.emoji;
+console.log(cat);
+
+console.log(Object.isFrozen(cat));
+console.log(Object.isSealed(dog));
+console.log(Object.isSealed(cat));
+
+// 확장 금지 preventExtensions : 추가 ❌
+const tiger = { name: '어흥' };
+Object.preventExtensions(tiger);
+console.log(Object.isExtensible(tiger));
+tiger.name = '야옹';
+console.log(tiger);
+delete tiger.name;
+console.log(tiger);
+tiger.age = 5;
+console.log(tiger);
